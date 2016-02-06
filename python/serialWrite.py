883a1e89271e75b42b7f9567da53839efc1ef3b7
Buck Baskin <mobile.wbaskin@gmail.com>
2016-02-06 01:07:45 -0500

    Echo works for now, moving to 'REPL'

--- python/serialWrite.py
+++ python/serialWrite.py
@@ -6,7 +6,7 @@ ser = serial.Serial('/dev/ttyACM1', 9600)
 
 for i in range(0,10):
     bytes_out = b''+str(i).encode('utf-8')+b'\r\n'
-    print('python set: '+str(bytes_out))
+    print('python set: '+str(bytes_out.decode('utf-8').strip()))
     ser.write(bytes_out)
-    print('python got: '+str(ser.readline()))
+    print('python got: '+str(ser.readline().decode('utf-8').strip()))
 
--- sketchbook/echoSerialTest/echoSerialTest.ino
+++ sketchbook/echoSerialTest/echoSerialTest.ino
@@ -8,6 +8,7 @@
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
 int led = 13;
+int counter = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -21,15 +22,22 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   if (Serial.available()) {
-    Serial.print("rcvd chars: ");
+    Serial.print("rcvd chars: >");
     digitalWrite(led, HIGH);
     char ch = Serial.read();
-    while(ch != '\n' && ch != '\xff') {
-      // Serial.print(ch);
-      delay(1000);
+    while(ch != '\n' && ch != '\xff' && ch != '\r') {
+      Serial.print(ch);
+      // delay(1000);
       ch = Serial.read();
     }
+    counter = 0;
+    while(ch == '\n' || ch == '\xff' || ch == '\r') {
+      ch = Serial.read();
+      if((counter++) > 1000) {
+        break;
+      }
+    }
     digitalWrite(led, LOW);
-    Serial.println(" and send end");
+    Serial.println("< and send end");
   }
 }
