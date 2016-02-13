403441704d727c7afaddd25f0152a7b24cbc004e
Buck Baskin <mobile.wbaskin@gmail.com>
2016-02-12 20:56:53 -0500

    Software Gimbal compiles!

--- sketchbook/SoftwareGimbal/IMU_Types.h
+++ sketchbook/SoftwareGimbal/IMU_Types.h
@@ -1,4 +1,12 @@
 #ifndef IMU_TYPES_H
 #define IMU_TYPES_H
 
+struct Vector3 {
+  float x;
+  float y;
+  float z;
+};
+
+typedef struct Vector3  Vector3;
+
 #endif
--- sketchbook/SoftwareGimbal/SoftwareGimbal.ino
+++ sketchbook/SoftwareGimbal/SoftwareGimbal.ino
@@ -1,5 +1,9 @@
 #include <MatrixMath.h>
+#include <rate.h>
 #include "IMU_Types.h"
+
+Rate imu_rate(10.0); // rate to poll IMU, hopefully will make this
+Rate control_rate(100.0); // rate to update control feedback, probably won't make 100hz
 
 void setup() {
   // put your setup code here, to run once:
@@ -8,6 +12,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (control_rate.q()) {
+    // control_update();
+    // filter motion update();
+  }
+  if (imu_rate.q()) {
+    // poll_imu(accel struct*, gyro struct*, msg struct*)
+    // filter sensor update();
+  }
 }
 
